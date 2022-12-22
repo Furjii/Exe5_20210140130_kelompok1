f841d23c0dd8bbfbbbdf54f503dd10a3ae74c24a
Furji UMY <furji.ft21@mail.umy.ac.id>
2022-12-22 16:15:59 +0700

    Add project files.

---
+++ Exe5_20210140130_kelompok1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe5_20210140130_kelompok1
+{
+    class queue
+    {
+        public int FUR, JI, max = 5;
+        public string user;
+        int[] queue_array = new int[5];
+    }
+
+    static void Main(string[] args)
+        {
+        }
+    }
+}
